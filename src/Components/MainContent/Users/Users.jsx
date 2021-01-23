de5a7fd4beb4b28b4ff1a886510a5b8683f15c5b
Neznayka <bugayow.andrei2014@gmail.com>
2021-01-23 20:17:29 +0200

    'fetch-users-promise'

--- src/Components/MainContent/Users/Users.jsx
+++ src/Components/MainContent/Users/Users.jsx
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import style from './Users.module.css';
 import {
-    Avatar, Divider,
+    Avatar, CircularProgress, Divider,
     IconButton,
     List,
     ListItem,
@@ -11,111 +11,45 @@ import {
     Typography
 } from "@material-ui/core";
 import {RiUserFollowLine, RiUserUnfollowLine} from "react-icons/all";
+import {fetchUsers, fetchUsersPromise} from "./utils";
+
 
 
 const Users = ({users,setUsers, follow, unfollow }) => {
-    let usersData = [{
-        id: 1,
-        fullName: 'Alexey B.',
-        status: 'I am a boss',
-        location: {city: 'Sumy', country: 'Ukraine'},
-        imgSrc: 'https://i.redd.it/wellr7jjiv011.jpg',
-        followed: false,
-    },
-        {
-            id: 2,
-            fullName: 'Andrey B.',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://chpic.su/_data/stickers/t/thelegendofaang/thelegendofaang_014.webp',
-            followed: false,
-        },
-        {
-            id: 3,
-            fullName: 'Egor S.',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://i.pinimg.com/736x/f5/ec/14/f5ec1493f8cf15a2f2d017ac9afe628d.jpg',
-            followed: false,
-        },
-        {
-            id: 4,
-            fullName: 'Alexandr H.',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://vignette.wikia.nocookie.net/avatar-the-last-airbender-discord/images/2/23/PFP2.jpg/revision/latest/top-crop/width/360/height/450?cb=20180911165028',
-            followed: false,
-        },
-        {
-            id: 5,
-            fullName: 'Michail G',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLyHlJd0ANpNKR2eD-EzQjJjDjBk1lHJOT_A&usqp=CAU',
-            followed: false,
-        },
-        {
-            id: 6,
-            fullName: 'Michail G',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLyHlJd0ANpNKR2eD-EzQjJjDjBk1lHJOT_A&usqp=CAU',
-            followed: false,
-        },
-        {
-            id: 7,
-            fullName: 'Michail G',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLyHlJd0ANpNKR2eD-EzQjJjDjBk1lHJOT_A&usqp=CAU',
-            followed: false,
-        },
-        {
-            id: 8,
-            fullName: 'Michail G',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLyHlJd0ANpNKR2eD-EzQjJjDjBk1lHJOT_A&usqp=CAU',
-            followed: false,
-        },
-        {
-            id: 9,
-            fullName: 'Michail G',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLyHlJd0ANpNKR2eD-EzQjJjDjBk1lHJOT_A&usqp=CAU',
-            followed: false,
-        },
-    ]
+    const [isLoading, setIsLoading] = useState(false);
 
-    // useEffect(()=> {
-    //     setUsers(usersData)
-    //     return () => {
-    //         setUsers([])
-    //     }
-    // }, [users])
-
-    if (!users.length) {
-        setUsers(usersData)
-    }
-
-
+    useEffect(()=> {
+        setIsLoading(true)
+        fetchUsersPromise()
+            .then((data) => {
+            setUsers(data)
+            setIsLoading(false)
+        })
+    }, [setUsers])
 
     return (
-        <List>
+        isLoading
+        ?<div className={style.progress}>
+                <CircularProgress />
+            </div>
+        :<List>
             {
                 users.map(user => <>
                     <ListItem className={style.user}>
                         <ListItemAvatar>
                             <Avatar>
-                                <img src={user.imgSrc} alt="Avatar" className='avatar'/>
+                                {
+                                    user.imgSrc
+                                        ?<img src={user.imgSrc} alt="Avatar" className='avatar'/>
+                                        :<Avatar className={style.avatarColor}>{user.name[0].toUpperCase()}</Avatar>
+                                }
                             </Avatar>
                         </ListItemAvatar>
                         <ListItemText primary={user.fullName} secondary={user.status}/>
 
                         <ListItemSecondaryAction style={{display: 'flex', alignItems: 'center'}}>
-                            <Typography
-                                variant='caption'>{`${user.location.country}, ${user.location.city}`}</Typography>
+                            {/*<Typography*/}
+                            {/*    variant='caption'>{`${user.location.country}, ${user.location.city}`}</Typography>*/}
                             {user.followed
                                 ? <IconButton color={"secondary"} children={<RiUserUnfollowLine/>}
                                               onClick={() => unfollow(user.id)}/>
---
+++ src/Components/MainContent/Users/utils.js
@@ -0,0 +1,30 @@
+export const fetchUsers = async (users, setUsers) => {
+    const response = await fetch("https://social-network.samuraijs.com/api/1.0/users?count=100")
+    const data = await response.json()
+    return data.items.map((user) => ({
+        ...user,
+        fullName: user.name,
+        imgSrc:user.photos.small,
+        location: {
+            country: '',
+            city: ''
+        }
+    }))
+}
+
+export const fetchUsersPromise = (users, setUsers) => {
+     return fetch("https://social-network.samuraijs.com/api/1.0/users?count=100")
+         .then(res => res.json())
+         .then(data => {
+        return data.items.map((user) => ({
+            ...user,
+            fullName: user.name,
+            imgSrc:user.photos.small,
+            location: {
+                country: '',
+                city: ''
+            }
+        }))
+    })
+
+}
