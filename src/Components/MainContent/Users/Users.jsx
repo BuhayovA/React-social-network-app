909e1ee68290a3977f9040eb21699cb1958691ac
Neznayka <bugayow.andrei2014@gmail.com>
2021-01-30 17:38:17 +0200

    'add-request-api'

--- src/Components/MainContent/Users/Users.jsx
+++ src/Components/MainContent/Users/Users.jsx
@@ -11,82 +11,10 @@ import {
     Typography
 } from "@material-ui/core";
 import {RiUserFollowLine, RiUserUnfollowLine} from "react-icons/all";
+import * as axios from "axios";
 
 
 const Users = ({users,setUsers, follow, unfollow }) => {
-    let usersData = [{
-        id: 1,
-        fullName: 'Alexey B.',
-        status: 'I am a boss',
-        location: {city: 'Sumy', country: 'Ukraine'},
-        imgSrc: 'https://i.redd.it/wellr7jjiv011.jpg',
-        followed: false,
-    },
-        {
-            id: 2,
-            fullName: 'Andrey B.',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://chpic.su/_data/stickers/t/thelegendofaang/thelegendofaang_014.webp',
-            followed: false,
-        },
-        {
-            id: 3,
-            fullName: 'Egor S.',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://i.pinimg.com/736x/f5/ec/14/f5ec1493f8cf15a2f2d017ac9afe628d.jpg',
-            followed: false,
-        },
-        {
-            id: 4,
-            fullName: 'Alexandr H.',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://vignette.wikia.nocookie.net/avatar-the-last-airbender-discord/images/2/23/PFP2.jpg/revision/latest/top-crop/width/360/height/450?cb=20180911165028',
-            followed: false,
-        },
-        {
-            id: 5,
-            fullName: 'Michail G',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLyHlJd0ANpNKR2eD-EzQjJjDjBk1lHJOT_A&usqp=CAU',
-            followed: false,
-        },
-        {
-            id: 6,
-            fullName: 'Michail G',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLyHlJd0ANpNKR2eD-EzQjJjDjBk1lHJOT_A&usqp=CAU',
-            followed: false,
-        },
-        {
-            id: 7,
-            fullName: 'Michail G',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLyHlJd0ANpNKR2eD-EzQjJjDjBk1lHJOT_A&usqp=CAU',
-            followed: false,
-        },
-            {
-            id: 8,
-            fullName: 'Michail G',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLyHlJd0ANpNKR2eD-EzQjJjDjBk1lHJOT_A&usqp=CAU',
-            followed: false,
-        },
-        {
-            id: 9,
-            fullName: 'Michail G',
-            status: 'I like prog, so much',
-            location: {city: 'Sumy', country: 'Ukraine'},
-            imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLyHlJd0ANpNKR2eD-EzQjJjDjBk1lHJOT_A&usqp=CAU',
-            followed: false,
-        },
-    ]
 
     // useEffect(()=> {
     //     setUsers(usersData)
@@ -95,9 +23,26 @@ const Users = ({users,setUsers, follow, unfollow }) => {
     //     }
     // }, [users])
 
-    if (!users.length) {
-        setUsers(usersData)
-    }
+    useEffect(() => {
+
+        if (!users.length) {
+
+            axios.get('https://social-network.samuraijs.com/api/1.0/users?count=20')
+                .then(response => {
+                    let users = response.data.items.map((user) => ({
+                        ...user,
+                        fullName: user.name,
+                        imgSrc:user.photos.small,
+                        location: {
+                            country: '',
+                            city: ''
+                        }
+                    }))
+                    setUsers(users)
+                })
+        }
+    }, [users])
+
 
 
 
@@ -108,7 +53,11 @@ const Users = ({users,setUsers, follow, unfollow }) => {
                     <ListItem className={style.user}>
                         <ListItemAvatar>
                             <Avatar>
-                                <img src={user.imgSrc} alt="Avatar" className='avatar'/>
+                                {
+                                    user.imgSrc
+                                        ? <img src={user.imgSrc} alt="Avatar" className='avatar'/>
+                                        : <Avatar className={style.colorIcon} >{user.fullName[0].toUpperCase()}</Avatar>
+                                }
                             </Avatar>
                         </ListItemAvatar>
                         <ListItemText primary={user.fullName} secondary={user.status}/>
